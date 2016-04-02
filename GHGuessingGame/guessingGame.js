15bd02d6956ad03a2de3aea362e7f89e1e4d909e
Danielle Gellis <danisyellis@gmail.com>
2016-04-02 03:33:55 -0700

    it does the things! (but why won't guesses go to 0?

--- GHGuessingGame/guessingGame.js
+++ GHGuessingGame/guessingGame.js
@@ -1,5 +1,6 @@
 var playersGuess = 0
 var winningNumber
+var guessesRemaining = 5;
 
 jQuery(document).ready(function(){
 /* **** Global Variables **** */
@@ -22,29 +23,61 @@ function playersGuessSubmission(event){
 		playersGuess = $("#guess").val();
 		$("#guess").val("");
 		console.log(playersGuess)
-}
-
-// Determine if the next guess should be a lower or higher number
-function lowerOrHigher(){
-	// add code here
+		checkGuess()
 }
 
 // Check if the Player's Guess is the winning number 
 
 function checkGuess(){
-	
+	if(Number(playersGuess) === winningNumber) {
+		$("h2").text("Winner, winner, chicken dinner!");
+		$("h1").text("Winner!");
+		$("h1").css({"font-style": "italic", "font-size": "65px", "background-color": "#9900ff"})
+	} else if(playersGuess > winningNumber) {
+		$("h1").after("<h3>You're too high</h3>")
+	} else if(playersGuess < winningNumber) {
+		$("h1").after("<h3>Too low!</h3>")
+	}	
+	guessesRemaining =guessesRemaining - 1;
+	showGuessesRemaining();
 }
 
+//
+function showGuessesRemaining(){
+		if(guessesRemaining > 0) {
+			$("p").text("You guessed " + playersGuess + " and you have " + guessesRemaining + " guesses remaining.");
+		} else {
+			$("body").css({"background-color": "red"})
+			$("h1").text("You Lost")
+		}
+}
 // Create a provide hint button that provides additional clues to the "Player"
 
-function provideHint(){
-	// add code here
+function provideHint() {
+	$("h2").before("<h4></h4>")
+	$("h4").text(checkHint())
+	$("h4").css("color", "#cc3300")
+}
+
+function checkHint() {
+	if(Math.abs(winningNumber - playersGuess) >= 30) {
+			return "You're way off";
+		}
+		else if(Math.abs(winningNumber - playersGuess) >= 20) {
+			return "Your guess is VERY COLD!";
+		}
+		else if(Math.abs(winningNumber - playersGuess) >= 10) {
+			return "You're getting close!";
+		}
+		else {
+			return "Wow, you're so close!";
+		}
 }
 
 // Allow the "Player" to Play Again
 
 function playAgain(){
-	// add code here
+	window.location.reload();
 }
 
 
@@ -52,7 +85,17 @@ function playAgain(){
 
 /* **** Event Listeners/Handlers ****  */
 
-	$("#submit").on("click", playersGuessSubmission)
+$("#submit").on("click", playersGuessSubmission);
+
+$(document).keypress(function(e) {
+    if(e.which == 13) {
+        playersGuessSubmission();
+    }
+});
+
+$("#playAgain").on("click", playAgain);
+
+$("#Hint").on("click", provideHint)
 
 
 })
